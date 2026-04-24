30356ad40913b7b84f85ebeba26a653c03310a9a
rpartho632-coder <rpartho632@gmail.com>
2026-04-24 17:42:47 +0600

    Program to find Even or Odd number

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ elif (b > a) and (b > c):
     print(f"Largest number b is {b}")
 else:
     print(f"Largest number c is {c}")
+
+
+n = int(input("Enter the number: "))
+if (n % 2 == 0):
+    print(f"Even number is {n}")
+else:
+    print(f"Odd number is {n}")
